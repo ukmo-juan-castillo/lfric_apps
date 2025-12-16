2ca168093915e543665d4fe25359f78e24e779a1
ukmo-juan.castillo <juan.m.castillo@metoffice.gov.uk>
2025-12-16 09:24:49 +0000

    Issue #48: run upgrade macro

--- applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
+++ applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
@@ -20,17 +20,15 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
 """
+
 
 class vn30_t48(MacroUpgrade):
     """Upgrade macro for ticket #48 by Juan M Castillo."""
@@ -39,6 +37,7 @@ class vn30_t48(MacroUpgrade):
     AFTER_TAG = "vn3.0_t48"
 
     def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-lfric2lfric
         self.add_setting(config, ["namelist:lfric2lfric", "mode"], "'ics'")
         self.add_setting(
             config,
