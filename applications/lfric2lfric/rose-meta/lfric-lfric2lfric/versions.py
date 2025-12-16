8420b6705b6ac9b49621cfafd6d129249633b89d
ukmo-juan.castillo <juan.m.castillo@metoffice.gov.uk>
2025-12-16 10:48:42 +0000

    Issue #48: passing again the update macros

--- applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
+++ applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
@@ -37,6 +37,7 @@ class vn30_t48(MacroUpgrade):
     AFTER_TAG = "vn3.0_t48"
 
     def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-lfric2lfric
         self.add_setting(config, ["namelist:lfric2lfric", "mode"], "'ics'")
         self.add_setting(
             config,
