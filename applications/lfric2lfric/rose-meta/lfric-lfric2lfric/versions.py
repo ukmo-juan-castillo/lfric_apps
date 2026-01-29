83a303e796f8745d5fbba86713ab43b6cbeb8ba5
ukmo-juan.castillo <juan.m.castillo@metoffice.gov.uk>
2026-01-29 07:29:26 +0000

    Apply upgrade macro

--- applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
+++ applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
@@ -20,13 +20,10 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
@@ -43,8 +40,8 @@ class vn30_t99(MacroUpgrade):
         # Commands From: rose-meta/lfric-lfric_atm
         """Set segmentation size for Gregory-Rowntree convection kernel"""
         self.add_setting(config, ["namelist:physics", "conv_gr_segment"], "16")
-
         return config, self.reports
+
 
 class vn30_t192(MacroUpgrade):
     """Upgrade macro for ticket #192 by Juan M. Castillo."""
@@ -53,5 +50,6 @@ class vn30_t192(MacroUpgrade):
     AFTER_TAG = "vn3.0_t192"
 
     def upgrade(self, config, meta_config=None):
-
+        # Commands From: rose-meta/lfric-lfric2lfric
+        # Blank Upgrade Macro
         return config, self.reports
